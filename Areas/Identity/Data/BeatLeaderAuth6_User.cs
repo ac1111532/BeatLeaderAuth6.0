a4c1fcc51caa2bff342ec0f6a69ce77836450601
Ethan Blanch <ac111532@avcol.school.nz>
2022-06-22 12:14:46 +1200

    Add project files.

---
+++ Areas/Identity/Data/BeatLeaderAuth6_User.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace BeatLeaderAuth6._0.Areas.Identity.Data;
+
+// Add profile data for application users by adding properties to the BeatLeaderAuth6_User class
+public class BeatLeaderAuth6_User : IdentityUser
+{
+}
+
